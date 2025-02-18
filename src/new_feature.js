3ac1bf1c20d53e9ddc5a295fae70e3ef1c634f0a
Giacomo Gagliano <giacomo.gagliano@me.com>
2025-02-18 17:57:10 +0100

    edit

--- src/new_feature.js
+++ src/new_feature.js
@@ -1,3 +1,40 @@
 export function newFeature() {
-    return "newFeature"
+  return "newFeature";
+}
+
+export function addRandomEmoji(str) {
+  const emojis = [
+    "😀",
+    "😂",
+    "😍",
+    "😎",
+    "😢",
+    "😡",
+    "🥳",
+    "🤔",
+    "😱",
+    "😴",
+    "😇",
+    "🤩",
+    "😜",
+    "😏",
+    "😬",
+    "😋",
+    "😳",
+    "😈",
+    "👻",
+    "💀",
+    "👽",
+    "🌈",
+    "🌟",
+    "🔥",
+    "🍀",
+    "🍉",
+    "🍕",
+    "🍔",
+    "🍩",
+    "🍭",
+  ];
+  const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+  return str + randomEmoji;
 }
